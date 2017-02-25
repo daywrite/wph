d14835b6b02d98c57d38716f35d71b794b7fd440
daywrite <daywrite@yeah.net>
2017-02-25 22:28:10 +0800

    基于bootstrap简单后台布局

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,23 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div id="app">   
+    <home></home> 
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Home from './components/Home'
 /* 引入jquery
    import jQuery from 'jquery'
    jQuery('#app').css('background-color', 'red') */
-import $ from 'jquery'
+// import $ from 'jquery'
 
 export default {
   components: {
-    Hello
+    Home
   },
   ready () {
     console.log('compiled')
-    $('#app').css('background-color', 'red')
+    // $('#app').css('background-color', 'red')
   }
 }
 </script>
