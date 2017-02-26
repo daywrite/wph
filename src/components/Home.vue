48c905b72ac06d8d324144de659ebce615cec972
daywrite <daywrite@yeah.net>
2017-02-26 21:26:20 +0800

    头部排序样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,12 @@
     <table class="table table-striped table-bordered table-hover">
       <thead>
         <tr>
-            <th>标题</th>
+            <th @click="sortBy">标题<span class="arrow" :class="sortOrderBool ? 'asc' : 'dsc'"></span></th>
             <th width="20%">发布时间</th>
             <th width="10%">作者</th>
             <th width="10%">回复数</th>
             <th width="10%">访问数</th>
+            <th width="10%">操作</th>
         </tr>
         </thead>
         <tbody>
@@ -18,6 +19,7 @@
             <td>{{item.author.loginname}}</td>
             <td>{{item.reply_count}}</td>
             <td>{{item.visit_count}}</td>
+            <td><button type="button" class="btn btn-danger">删除</button></td>
         </tr>
         </tbody>
     </table>
@@ -34,6 +36,7 @@ export default {
     return {
       allArticle: '',
       dataList: [],
+      sortOrderBool: false,
       pageModel: {
         url: 'https://cnodejs.org/api/v1/topics',
         menu: [5, 10, 20]
@@ -49,6 +52,11 @@ export default {
       }
     }
   },
+  methods: {
+    sortBy: function (key) {
+      this.sortOrderBool = !this.sortOrderBool
+    }
+  },
   events: {
     refresh: function (e) {
       this.dataList = e
@@ -62,4 +70,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.arrow {
+ display: inline-block;
+ vertical-align: middle;
+ width:0; 
+ height:0; 
+ margin-left: 5px;
+ opacity: 0.66;
+ /*border-left:4px solid transparent;
+ border-right:4px solid transparent;
+ border-bottom:4px solid green;*/
+}
+.arrow.asc {
+ border-left: 4px solid transparent;
+ border-right: 4px solid transparent;
+ border-bottom: 4px solid green;
+}
+ 
+.arrow.dsc {
+ border-left: 4px solid transparent;
+ border-right: 4px solid transparent;
+ border-top: 4px solid green;
+}
 </style>
