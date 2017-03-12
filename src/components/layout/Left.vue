cb56b5bcd7be828477cc65335b42c7759277a9d7
daywrite <daywrite@yeah.net>
2017-03-12 22:02:26 +0800

    侧边栏结合jquery

--- src/components/layout/Left.vue
+++ src/components/layout/Left.vue
@@ -3,13 +3,47 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-3 col-md-2 sidebar">
-        <ul class="nav nav-sidebar">
-            <li><a v-link="{ path: '/home' }">首页</a></li>
-            <li><a v-link="{ path: '/hello' }">欢迎</a></li>
-            <li v-for='el in data'><a ><i class="fa fa-home"></i><span class="nav-label">vuex动态改变：{{el.name}}</span></a></li>
-            <li><a><i class="fa fa-home"></i>{{countValue}}</a></li>
-            <li><a v-link="{ path: '/vuext' }">vuex</a></li>
-        </ul>       
+       <ul id="accordion" class="accordion">     
+    <li>
+        <div class="link" @click="dropd"><i class="glyphicon glyphicon-th"></i>基础数据<i class="glyphicon glyphicon-chevron-up"></i></div>
+        <ul class="submenu">
+          <li><a v-link="{ path: '/home' }">首页</a></li>
+          <li><a v-link="{ path: '/hello' }">欢迎</a></li>
+          <li v-for='el in data'><a ><i class="fa fa-home"></i><span class="nav-label">vuex动态改变：{{el.name}}</span></a></li>
+          <li><a><i class="fa fa-home"></i>{{countValue}}</a></li>
+          <li><a v-link="{ path: '/vuext' }">vuex</a></li>
+        </ul>
+    </li>
+    <li>
+        <div class="link" @click="dropd"><i class="glyphicon glyphicon-th"></i>一级分类<i class="glyphicon glyphicon-chevron-up"></i></div>
+        <ul class="submenu">
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+        </ul>
+    </li>
+    <li>
+        <div class="link" @click="dropd"><i class="glyphicon glyphicon-th"></i>一级分类<i class="glyphicon glyphicon-chevron-up"></i></div>
+        <ul class="submenu">
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+        </ul>
+    </li>
+    <li>
+        <div class="link" @click="dropd"><i class="glyphicon glyphicon-th"></i>一级分类<i class="glyphicon glyphicon-chevron-up"></i></div>
+        <ul class="submenu">
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+            <li><a href="http://www.jq22.com">jQuery插件库</a></li>
+        </ul>
+    </li>
+</ul>     
       </div>
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <slot></slot>
@@ -19,12 +53,20 @@
 </template>
 
 <script>
+import $ from 'jquery'
 import store from '../vuex/store'
 export default {
   name: 'left',
   data () {
     return {
       msg: 'Hello World!'
+    }
+  },
+  methods: {
+    dropd: function (event) {
+      $(event.currentTarget).next().slideToggle()
+      $(event.currentTarget).parent().toggleClass('open')
+      $('#accordion').find('.submenu').not($(event.currentTarget).next()).slideUp().parent().removeClass('open')
     }
   },
   store: store,
@@ -41,13 +83,103 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* 菜单选中颜色 */
-ul li .active{
-  color:#42b983;
+ul {
+    list-style-type: none;
+    padding:0;
 }
-/* Hide for mobile, show later */
-.sidebar {
-  display: none;
+a {
+    color: #b63b4d;
+    text-decoration: none;
+}
+h1 {
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 400;
+    text-align: center;
+    margin-top: 80px;
+}
+
+h1 a {
+    color: #c12c42;
+    font-size: 16px;
+}
+
+.accordion {
+    width: 100%;
+    max-width: 360px;
+    background: #FFF;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+}
+.accordion .link {
+    cursor: pointer;
+    display: block;
+    padding: 15px 15px 15px 42px;
+    color: #4D4D4D;
+    font-size: 14px;
+    font-weight: 700;
+    border-bottom: 1px solid #CCC;
+    position: relative;
+    -webkit-transition: all 0.4s ease;
+    -o-transition: all 0.4s ease;
+    transition: all 0.4s ease;
+}
+.accordion li:last-child .link {
+    border-bottom: 0;
+}
+.accordion li i {
+    position: absolute;
+    top: 16px;
+    left: 12px;
+    font-size: 18px;
+    color: #595959;
+    -webkit-transition: all 0.4s ease;
+    -o-transition: all 0.4s ease;
+    transition: all 0.4s ease;
+}
+.accordion li i.glyphicon-chevron-up {
+    right: 12px;
+    left: auto;
+    font-size: 16px;
+}
+
+.accordion li.open .link {
+    color: #b63b4d;
+}
+
+.accordion li.open i {
+    color: #b63b4d;
+}
+.accordion li.open i.glyphicon-chevron-up {
+    -webkit-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    transform: rotate(180deg);
+}
+
+.submenu {
+    display: none;
+    background: #444359;
+    font-size: 14px;
+}
+.submenu li {
+    border-bottom: 1px solid #4b4a5e;
+}
+.submenu a {
+    display: block;
+    text-decoration: none;
+    color: #d9d9d9;
+    padding: 12px;
+    padding-left: 42px;
+    -webkit-transition: all 0.25s ease;
+    -o-transition: all 0.25s ease;
+    transition: all 0.25s ease;
+}
+
+.submenu a:hover {
+    background: #b63b4d;
+    color: #FFF;
 }
 @media (min-width: 768px) {
   .sidebar {
@@ -64,22 +196,7 @@ ul li .active{
     border-right: 1px solid #eee;
   }
 }
-/* Sidebar navigation */
-.nav-sidebar {
-  margin-right: -21px; /* 20px padding + 1px border */
-  margin-bottom: 20px;
-  margin-left: -20px;
-}
-.nav-sidebar > li > a {
-  padding-right: 20px;
-  padding-left: 20px;
-}
-.nav-sidebar > .active > a,
-.nav-sidebar > .active > a:hover,
-.nav-sidebar > .active > a:focus {
-  color: #fff;
-  background-color: #428bca;
-}
+
 /*
  * Main content
  */
